b892ca2a91299ed9adf9bf250e13e95487d6158c
JoshWarn <Computer@DESKTOP-3EHEOAE>
2023-08-25 11:21:44 -0400

    Testing

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("This is just a basic test of pushing to GitHub Attempt 2.")
+import numpy as np
+import skimage
+import warnings
+class generate_multilabel_toy_dataset(sample_number=1000, x_res=256, y_res=256, channels=3, v_min=0, v_max=1,
+                                      label_count=4, label_frequency=0.5, path=None, save_to_folder = False):
+    """
+    Creates a dataset with basic shapes; 1, 2, 3, 4... = circle, line, triangle, square etc...
+
+    sample_number   - number of samples to generate
+    x_res, y_res    - dimensions of generated image
+    channels        - number of image channels to use
+    v_min, v_max    - minimum and maximum values used in channels (typically 0-1 for samples).
+
+    label_count     - number of classes to generate dataset with
+    label_frequency - how frequently to have a label occur in an image
+    path            - where to save the dataset to
+    save_to_folder  - if data should be saved
+    """
+
+    # make blank numpy array
+
+    if (v_min != 0 or v_max != 1) and save_to_folder == True:
+        warnings.warr(f"v_min and v_max of 0-1 should be used when saving images!"
+                      f"v_min and v_max of {v_min} and {v_max} found.")
+
+
+    for i in range(sample_number):
